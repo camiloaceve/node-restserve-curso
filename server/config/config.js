084ad00044fa5a71067da606e1239c08a5371601
camiloaceve <acevedomoralescamilo@gmail.com>
2018-10-19 08:28:34 -0500

    Productos y categorias

--- server/config/config.js
+++ server/config/config.js
@@ -11,13 +11,30 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 // ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = '48h';
+
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+// ============================
 //  Base de datos
 // ============================
 let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/curso';
 } else {
-    urlDB = 'mongodb://curso:curso123@ds111489.mlab.com:11489/curso';
+    // urlDB = 'mongodb://curso:curso123@ds111489.mlab.com:11489/curso';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDBB = urlDB;
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,137 +1,61 @@
-const express = require('express');
-const _ = require('underscore');
-const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 
-const Usuario = require('../models/usuario');
-
-const app = express();
-
-
-app.get('/usuarios', function (req, res) {
-    
-    let desde = req.query.desde || 0;
-    desde = Number(desde);
-
-    let limite = req.query.limit || 5;
-    limite = Number(limite);
-
-    Usuario.find({ estado: true})
-            .skip(desde)
-            .limit(limite)
-            .exec( (err, usuario) =>{
-
-                if (err) {
-                    return res.status(400).json({
-                        ok: false,
-                        err
-                    });
-                }
-
-                Usuario.count({ estado: true }, (err, conteo) =>{
-                    
-                    res.json({
-                        ok: true,
-                        usuario,
-                        cuantos: conteo
-                    });
-                });
-
-            })
-
-  });
-  
-  app.post('/usuarios', function (req, res) {
-  
-      let body = req.body;
-
-      let usuario = new Usuario({
-          nombre: body.nombre,
-          email: body.email,
-         password: bcrypt.hashSync(body.password, 10),
-          role: body.role,
-      });
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+};
 
 
-      usuario.save((err, usuarioDB) =>{
-        
-        if(err) {
-            res.status(400).json({
-                ok: false,
-                err
-            });
-        }
-
-        res.json({
-            ok: true,
-            usuario: usuarioDB
-        });
-    });
-});
-  
-  app.put('/usuarios/:id', function (req, res) {
-  
-      let id = req.parms.id;
-      let body = _.pick(req.body, ['nombre','email','img','role','estado']);
-
-      Usuario.findByIdAndUpdate( id, body, { new: true, runValidators: true }, (err, usuarioDB) =>{
-
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        }
+let Schema = mongoose.Schema;
 
 
-
-          res.json({
-              ok: true,
-              usuario: usuarioDB
-          });
-      })
-  
-  });
-  
-  app.delete('/usuarios', function (req, res) {
-
-
-    let id = req.params.id;
-
-    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
-
-    let cambiaEstado = {
-        estado: false
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
     }
-
-    Usuario.findByIdAndRemove(id, cambiaEstado, {new: true}, (err, usuarioBorrado) => {
-
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
-            });
-        };
-
-        if ( !usuarioBorrado  ){
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'Usuario no encontrado'
-                }
-            });
-        }
-
-        res.json({
-            ok: true,
-            usuario: usuarioBorrado
-        });
-
-    })
+});
 
 
+usuarioSchema.methods.toJSON = function() {
 
-   });
-  
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
 
-  module.exports = app;
+    return userObject;
+}
+
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
